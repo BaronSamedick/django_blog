83b07344969c999e5702a0fb6c7ba414bff0718c
ds.eruslanov <ds.eruslanov@tensor.ru>
2024-01-25 14:45:11 +0500

    * добавлена пагинация для страниц статей и категорий * добавлен редирект на uri article при запросе главное страницы * мелкие правки

--- src/apps/blog/views.py
+++ src/apps/blog/views.py
@@ -2,11 +2,14 @@ from django.views.generic import DetailView, ListView
 
 from .models import Article, Category
 
+PAGINATION_SIZE = 5
+
 
 class ArticleListView(ListView):
     model = Article
     template_name = "blog/articles_list.html"
     context_object_name = "articles"
+    paginate_by = PAGINATION_SIZE
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -29,6 +32,7 @@ class ArticleByCategoryListView(ListView):
     model = Article
     template_name = "blog/articles_list.html"
     context_object_name = "articles"
+    paginate_by = PAGINATION_SIZE
     category = None
 
     def get_queryset(self):
